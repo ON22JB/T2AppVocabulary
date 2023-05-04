7492484a5750b739de919383522379159f5abb45
ON22JB <jacqbohn@aol.com>
2023-05-04 10:01:58 +0200

    Save and Reset functions

--- src/index.ts
+++ src/index.ts
@@ -151,17 +151,71 @@ function reloadVocabEn() {
     }
   }
 
-  //Delete Vocab pairs
-  //Here is space for new function to delete single vocab entrees
+//Save and Reset buttons
+  const saveBtn = document.querySelector("#saveBtn") as HTMLButtonElement;
+  const resetBtn = document.querySelector("#resetBtn") as HTMLButtonElement;
   
-  function changeOnClick() {
-    console.log("test");
-    vocabDe.forEach((vocabDe) => {
-    const oneVocabDeContainer = document.createElement("div");
-    oneVocabDeContainer.innerHTML = "";
-    oneVocabDeContainer.style.backgroundColor = "red";
-    })
-  } 
+//start of En functions
+  let textEn = document.getElementById('vocabEnContainer');
+  if (textEn){
+    let myDataEn;
+  let postDataEn = window.localStorage.getItem("save");
+  if (postDataEn == null || postDataEn == '') {
+    myDataEn = textEn.innerHTML;
+      window.localStorage.setItem("save", myDataEn);
+  } else {
+      textEn.innerHTML = postDataEn;
+  }}
+
+//save En Vocab List on reload of page
+  function saveChangesEn() {
+      let textEn = document.getElementById('vocabEnContainer');
+      let myDataEn;
+      if (textEn){
+        myDataEn = textEn.innerHTML;
+      window.localStorage.setItem("save", myDataEn);
+      }
+  }
+
+//clear local storage En
+  function clearStorageEn() {
+    let textEn = document.getElementById('vocabEnContainer');
+    if (textEn){
+    let reset = textEn.innerHTML;
+    window.localStorage.clear();
+    textEn.innerHTML = reset;
+}}
+
+//start of De functions
+let textDe = document.getElementById('vocabDeContainer');
+if (textDe){
+  let myDataDe;
+let postDataDe = window.localStorage.getItem("save");
+if (postDataDe == null || postDataDe == '') {
+  myDataDe = textDe.innerHTML;
+    window.localStorage.setItem("save", myDataDe);
+} else {
+    textDe.innerHTML = postDataDe;
+}}
+
+//save De Vocab List on reload of page
+function saveChangesDe() {
+      let textDe = document.getElementById('vocabDeContainer');
+      let myDataDe;
+      if (textDe){
+        myDataDe = textDe.innerHTML;
+      window.localStorage.setItem("save", myDataDe);
+      }
+  }
+
+//clear local storage De
+function clearStorageDe() {
+  let textDe = document.getElementById('vocabDeContainer');
+  if (textDe){
+  let reset = textDe.innerHTML;
+  window.localStorage.clear();
+  textDe.innerHTML = reset;
+}}
 
 //Open fullscreen
 let elem = document.documentElement as HTMLElement;
@@ -189,7 +243,10 @@ function initApp() {
     newVocabDeInput.addEventListener("keydown", enterKeyOnVocabInput);
     emptyVocabBtn.addEventListener("click", emptyVocabList);
     fullScBtn.addEventListener("click", openFullscreen);
-    vocabDeContainer.addEventListener("click", changeOnClick);
+    saveBtn.addEventListener("click", saveChangesEn);
+    saveBtn.addEventListener("click", saveChangesDe);
+    resetBtn.addEventListener("click", clearStorageEn);
+    resetBtn.addEventListener("click", clearStorageDe);
   }
   
   initApp();
